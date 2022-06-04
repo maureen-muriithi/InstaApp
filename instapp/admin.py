21e52dcb4fac6668e79f726fb92ab9827b2e79cd
Maureen-Muriithi <moh2wanja@gmail.com>
2022-06-04 16:56:04 +0300

    Change admin header and title

--- instapp/admin.py
+++ instapp/admin.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from .models import Post,Comment
 
 # Register your models here.
+admin.site.site_header = 'InstaApp - Administration'
 admin.site.register(Post)
 admin.site.register(Comment)
