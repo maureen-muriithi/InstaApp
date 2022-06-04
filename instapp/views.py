2985d93f9d715bef1567dd24a060611e6f1ec109
Maureen-Muriithi <moh2wanja@gmail.com>
2022-06-05 00:42:31 +0300

    Enable sending of email to new users

--- instapp/views.py
+++ instapp/views.py
@@ -2,12 +2,16 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 import datetime as dt
 from .models import Post
+from .email import send_welcome_email
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 def index(request):
     title = 'Welcome to Instagram'
     return render(request, 'posts/index.html', {"title": title})
 
+@login_required(login_url='/accounts/login/')
 def display_posts(request):
     date = dt.date.today()
     posts = Post.objects.all()
